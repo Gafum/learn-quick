09825426c07ed4eed9e17d78b7ae3a053b8b22e5
Marchuk Dima <marcukdyma@gmail.com>
2024-10-19 19:52:36 +0200

    added styles to combining screen

--- src/Function/shufleArray.ts
+++ src/Function/shufleArray.ts
@@ -1,9 +1,11 @@
 export function shuffleArray<T>(array: T[]): T[] {
-   for (let i = array.length - 1; i > 0; i--) {
+   let localArray = JSON.parse(JSON.stringify(array));
+
+   for (let i = localArray.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      const temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
+      const temp = localArray[i];
+      localArray[i] = localArray[j];
+      localArray[j] = temp;
    }
-   return array;
+   return localArray;
 }
--- src/Screens/CombiningScreen/CombiningScreen.tsx
+++ src/Screens/CombiningScreen/CombiningScreen.tsx
@@ -1,40 +1,45 @@
 import { useEffect, useState } from "react";
 import useTestData from "../../Hooks/useTestData";
+import styles from "./CombiningScreen.module.scss";
+import { IWordData } from "../../Types/interfaces";
 import { shuffleArray } from "../../Function/shufleArray";
 
 function CombiningScreen(): JSX.Element {
    const { myIterableList } = useTestData()
-   const [chooseList, setChooseList] = useState(myIterableList)
+   const [chooseList, setChooseList] = useState<IWordData[]>([])
 
 
    function startTest() {
-      setChooseList(shuffleArray(myIterableList))
+      setChooseList(
+         shuffleArray([...myIterableList])
+      )
    }
 
    useEffect(() => {
       startTest()
-   }, [])
+   }, [myIterableList])
 
    return (
-      <div style={{ display: "flex", gap: "10px" }}>
-         <div>
+      <div className={styles.combiningScreen}>
+         <div className={styles.leftSide}>
             {
                myIterableList.map((elem) => {
                   return (
-                     <div>
+                     <button key={elem.id + "left"} className={styles.oneBlock}>
                         {elem.word}
-                     </div>
+                     </button>
                   )
                })
             }
          </div>
-         <div>
+
+         <div className={styles.rightSide}>
             {
                chooseList.map((elem) => {
                   return (
-                     <div>
+                     <button key={elem.id + "right"} className={styles.oneBlock}>
                         {elem.meaning}
-                     </div>
+                     </button>
                   )
                })
             }
