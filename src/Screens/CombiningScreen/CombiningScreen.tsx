685107e0027a64a39edb26af2d487648552a7cb1
Marchuk Dima <marcukdyma@gmail.com>
2024-10-20 17:39:18 +0200

    show wrong answer

--- src/Screens/CombiningScreen/CombiningScreen.tsx
+++ src/Screens/CombiningScreen/CombiningScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import useTestData from "../../Hooks/useTestData";
 import styles from "./CombiningScreen.module.scss";
 import { IWordData, NumStr } from "../../Types/interfaces";
@@ -6,13 +6,18 @@ import { shuffleArray } from "../../Function/shufleArray";
 import { createClasses } from "../../Function/createClasses";
 import { findIndexOfELement } from "../../Function/findElementByID";
 import CustomBtn from "../../UI/CustomBtn/CustomBtn";
+import { m, LazyMotion, domAnimation } from "framer-motion"
+import { ScreensAnimation } from "../../CustomData/animation";
+
+interface IWordDataCombiningScreen extends IWordData {
+   wrongAnswer?: boolean;
+}
 
 function CombiningScreen(): JSX.Element {
    const { myIterableList } = useTestData()
-   const [leftSideList, setLeftSideList] = useState<IWordData[]>([])
-   const [rightSideList, setRightSideList] = useState<IWordData[]>([])
+   const [leftSideList, setLeftSideList] = useState<IWordDataCombiningScreen[]>([])
+   const [rightSideList, setRightSideList] = useState<IWordDataCombiningScreen[]>([])
    const [selectedElement, setSelectedElement] = useState<NumStr>(-1);
-
 
    useEffect(() => {
       startTest()
@@ -27,11 +32,24 @@ function CombiningScreen(): JSX.Element {
       )
    }
 
-   function checkAnswer(event: React.MouseEvent, choosedElement: NumStr) {
-      function removeChoosedElement(prev: IWordData[]) {
+   function selectFirstElem(elementId: NumStr) {
+      setSelectedElement(elementId)
+      setRightSideList(prev => removeWrongAnswer(prev))
+   }
+
+   function removeWrongAnswer(localList: IWordDataCombiningScreen[]) {
+      //Remove wrongAnswer parameter in list
+
+      return localList.map((elem) => {
+         return { ...elem, ...{ wrongAnswer: false } }
+      });
+   }
+
+   function checkAnswer(choosedElement: NumStr) {
+      function removeChoosedElement(prev: IWordDataCombiningScreen[]) {
          let localList: IWordData[] = JSON.parse(JSON.stringify(prev))
          localList.splice(findIndexOfELement(localList, choosedElement), 1)
-         return localList;
+         return removeWrongAnswer(localList);
       }
 
       if (selectedElement == -1) return;
@@ -41,10 +59,15 @@ function CombiningScreen(): JSX.Element {
          setLeftSideList(removeChoosedElement)
          setSelectedElement(-1)
       } else {
-         (event.currentTarget as HTMLButtonElement).style.backgroundColor = "red"
+         setRightSideList(prev => {
+            let localList: IWordDataCombiningScreen[] = JSON.parse(JSON.stringify(prev))
+            localList[findIndexOfELement(localList, choosedElement)].wrongAnswer = true;
+            return localList;
+         })
       }
    }
 
+   //Loading
    if (myIterableList.length == 0) {
       return (
          <div style={{ display: "flex", justifyContent: "center" }}>
@@ -53,51 +76,64 @@ function CombiningScreen(): JSX.Element {
       )
    }
 
+   //Restart Test
    if (leftSideList.length == 0) {
-      return (<div style={{ display: "flex", justifyContent: "center" }}>
-         <CustomBtn onClick={startTest}>
-            Repeate Test
-         </CustomBtn>
-      </div>)
+      return (
+         <div style={{ display: "flex", justifyContent: "center" }}>
+            <CustomBtn onClick={startTest}>
+               Repeate Test
+            </CustomBtn>
+         </div>
+      )
    }
 
    return (
-      <div className={styles.combiningScreen}>
-         <div className={styles.leftSide}>
-            {
-               leftSideList.map((elem) => {
-                  return (
-                     <button
-                        onClick={() => setSelectedElement(elem.id)}
-                        key={elem.id + "left"}
-                        className={
-                           createClasses([
-                              styles.oneBlock,
-                              selectedElement == elem.id && styles.selectedElement
-                           ])}>
-                        {elem.word}
-                     </button>
-                  )
-               })
-            }
-         </div>
+      <LazyMotion features={domAnimation}>
+         <m.div
+            {...ScreensAnimation}
+            className={styles.combiningScreen}
+         >
+            <div className={styles.leftSide}>
+               {
+                  leftSideList.map((elem) => {
+                     return (
+                        <button
+                           onClick={() => selectFirstElem(elem.id)}
+                           key={elem.id + "left"}
+                           className={
+                              createClasses([
+                                 styles.oneBlock,
+                                 selectedElement == elem.id && styles.selectedElement
+                              ])}>
+                           {elem.word}
+                        </button>
+                     )
+                  })
+               }
+            </div>
 
-         <div className={styles.rightSide}>
-            {
-               rightSideList.map((elem) => {
-                  return (
-                     <button
-                        key={elem.id + "right"}
-                        className={styles.oneBlock}
-                        onClick={(event) => checkAnswer(event, elem.id)}
-                     >
-                        {elem.meaning}
-                     </button>
-                  )
-               })
-            }
-         </div>
-      </div>
+            <div className={styles.rightSide}>
+               {
+                  rightSideList.map((elem) => {
+                     return (
+                        <button
+                           key={elem.id + "right"}
+                           className={
+                              createClasses([
+                                 styles.oneBlock,
+                                 elem.wrongAnswer ? styles.wrongAnswer : "",
+                              ])
+                           }
+                           onClick={() => checkAnswer(elem.id)}
+                        >
+                           {elem.meaning}
+                        </button>
+                     )
+                  })
+               }
+            </div>
+         </m.div>
+      </LazyMotion>
    );
 }
 
